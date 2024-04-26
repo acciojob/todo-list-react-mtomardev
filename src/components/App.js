72bff5ffad7158c41d1be253fb1cfed4fe5fca0c
mtomardev <120013813+mtomardev@users.noreply.github.com>
2024-04-26 23:23:14 +0530

    try

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [inputData, setInputData] = useState("")
+  let [output, setOutput] = useState("")
+    function outputHandler(){
+      setOutput(inputData)
+    }
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>To Do List</h1>
+        <input type="text" onChange={(e) => setInputData(e.target.value)}/>
+        <button onClick={outputHandler} >Add</button>
+        {
+          output && <div>{output} <button>Edit</button><button>Delete</button></div>
+        }
+
+
+
+
     </div>
   )
 }
