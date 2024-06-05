2e3175d2f5a2b1a6c4812ddb914cf562e398fab8
mtomardev <120013813+mtomardev@users.noreply.github.com>
2024-06-05 16:59:37 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,142 @@
+// import React,{useState} from "react";
+// import './../styles/App.css';
 
-import React,{useState} from "react";
-import './../styles/App.css';
+// const App = () => {
+//   // let [inputData, setInputData] = useState("")
+//   // let [output, setOutput] = useState("")
+//   //   function outputHandler(){
+//   //     setOutput(inputData)
+//   //   }
+//   const [inputData, setInputData] = useState("");
+//   const [todoList, setTodoList] = useState([]);
+//   const [editIndex, setEditIndex] = useState();
+//   const [updateInput, setUpdateInput] = useState('');
 
-const App = () => {
-  let [inputData, setInputData] = useState("")
-  let [output, setOutput] = useState("")
-    function outputHandler(){
-      setOutput(inputData)
-    }
-  return (
-    <div>
-        {/* Do not remove the main div */}
-        <h1>To Do List</h1>
-        <input type="text" onChange={(e) => setInputData(e.target.value)}/>
-        <button onClick={outputHandler} >Add</button>
-        {
-          output && <div>{output} <button>Edit</button><button>Delete</button></div>
+//   function handleAddTodo(){
+    
+//     if(inputData !== ''){
+//       setTodoList([...todoList, inputData])
+    
+//     }
+    
+    
+    
+//   }
+
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//         {/* <h1>To Do List</h1> */}
+//         {/* <input type="text" onChange={(e) => setInputData(e.target.value)}/>
+//         <button onClick={outputHandler} >Add</button>
+//         {
+//           output && <div>{output} <button>Edit</button><button>Delete</button></div>
+//         } */}
+
+//         <h1>To Do List</h1>
+//         {/* <input type="text" placeholder="Enter To do" onChange={(e) => setInputData(e.target.value)}/>
+//         <button onClick={handleAddTodo}>Add</button>
+//         <ul>
+//         {
+//           todoList.map((todoList, index) =>{
+//             <li key={index}>
+//               {
+//                  editIndex === index ? (
+//                   <>
+//                   </>
+//                  ):(<>
+//                   {todoList}
+//                   <button>Edit</button>
+                  
+//                   </>
+//                  )
+//                 }
+            
+//             <button>Delete</button>  
+//             </li>
+//           })
+//         }
+//         </ul>
+//  */}
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React, {useState} from 'react'
+
+const Todo2 = () =>{
+    const[todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    const [editIndex, setEditIndex] = useState();
+    const [updateInput, setUpdateInput] = useState('');
+
+    function handleAddTodo(){
+        if(inputValue.trim() !== ''){
+            setTodos([...todos, inputValue])
+            setInputValue('')
         }
+    }
+
+    function handleDelete(index){
+        const newTodo = [...todos];
+        newTodo.splice(index, 1);
+        setTodos(newTodo)
+    }
+
+    function handleEdit(index){
+        setEditIndex(index);
+        setUpdateInput(todos[index])
+    }
+
+    function handleUpdateTodo(index){
+        if(updateInput.trim() !== '' ){
+            const newTodo = [...todos]
+            newTodo[index] = updateInput;
+            setTodos(newTodo);
+            setEditIndex();
+            setUpdateInput('')
+        }
+    }
+
+    return(
+        <div>
+            <h1>Handle Todo ADD Edit Delet</h1>
+            <input type='text' placeholder='Enter your List' value={inputValue} onChange={(e)=> setInputValue(e.target.value)}/>
+            <button onClick={handleAddTodo}>Add Todo</button>
+            <ul>
+                {
+                    todos.map((todos, index)=>{
+                        return(
+                        <li key={index}>
+                          {
+                            editIndex === index ?(
+                                <>
+                                <input type='text' placeholder='Update Text' 
+                                value={updateInput}
+                                onChange={(e)=>setUpdateInput(e.target.value)}/>
+                                <button onClick={() => handleUpdateTodo(index)}>Update</button>
+                                </>
+                            ) : (
+                                <>
+                                    {todos}
+                                    <button onClick={()=> handleEdit(index)}>Edit</button>    
+                                </>
+                            )
+                          }      
+                            
+                            <button onClick={() => handleDelete(index)}>Delet</button>
+                        </li>
+                        )
+                    })
+                }
 
 
-
-
-    </div>
-  )
+                </ul>            
+        </div>
+    )
 }
-
-export default App
+export default Todo2;
